a8422c11d5ba2f75199cbbb108f6bf763ccf394c
Pedro Avalos <pavalos6401@gmail.com>
2023-01-16 16:03:10 -0600

    fix hooks.py for newer version of Window class

--- config/qtile/src/hooks.py
+++ config/qtile/src/hooks.py
@@ -3,6 +3,7 @@
 import subprocess
 
 from libqtile import hook
+from libqtile.backend.base import Window
 
 from .util import paths
 
@@ -14,25 +15,24 @@ def autostart() -> None:
     subprocess.call([paths.scripts_dir / "autostart.sh"])
 
 
-# TODO: Fix these to work with wayland, they seem to bring up exceptions
-
-
 @hook.subscribe.client_new
-def float_firefox(window) -> None:
+def float_firefox(window: Window) -> None:
     """Floats firefox windows such as dialogs and prompts."""
 
-    wm_class = window.window.get_wm_class()
-    w_name = window.window.get_name()
+    info: dict = window.info()
+    wm_class = info["wm_class"]
+    w_name = info["name"]
     if wm_class == ("Places", "firefox") and w_name == "Library":
         window.floating = True
 
 
 @hook.subscribe.client_new
-def float_pycharm(window) -> None:
+def float_pycharm(window: Window) -> None:
     """Floats dialog windows in PyCharm."""
 
-    wm_class = window.window.get_wm_class()
-    w_name = window.window.get_name()
+    info: dict = window.info()
+    wm_class = info["wm_class"]
+    w_name = info["name"]
     if (
         wm_class == ("jetbrains-pycharm-ce", "jetbrains-pycharm-ce") and w_name == " "
     ) or (wm_class == ("java-lang-Thread", "java-lang-Thread") and w_name == "win0"):
@@ -40,16 +40,16 @@ def float_pycharm(window) -> None:
 
 
 @hook.subscribe.client_new
-def float_steam(window) -> None:
+def float_steam(window: Window) -> None:
     """Floats windows in Steam such as friends list."""
 
-    wm_class = window.window.get_wm_class()
-    w_name = window.window.get_name()
+    info: dict = window.info()
+    wm_class = info["wm_class"]
+    w_name = info["name"]
     if wm_class == ("Steam", "Steam") and (
         w_name != "Steam"
         or w_name == "Friends List"
         or w_name == "Screenshot Uploader"
         or w_name.startswith("Steam - News")
-        or "PMaxSize" in window.window.get_wm_normal_hints().get("flags", ())
     ):
         window.floating = True
