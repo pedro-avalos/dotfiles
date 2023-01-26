d2837a4e9a38885aae12e657e9f8adb84ca1806c
Pedro Avalos <pavalos6401@gmail.com>
2023-01-25 23:33:56 -0600

    work on emacs configuration

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -2,20 +2,6 @@
 ;; file: init.el
 ;; Do not edit init.el. Edit config.org instead.
 
-(org-babel-load-file
- (expand-file-name
-  "config.org"
-  user-emacs-directory))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(evil-tutor general evil-collection evil doom-modeline all-the-icons use-package no-littering)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; My configuration is written in org-mode
+(require 'org)
+(org-babel-load-file "~/.emacs.d/config.org")
