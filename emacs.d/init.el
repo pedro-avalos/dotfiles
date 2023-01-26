55e97e9bddb88813653899478fb1a832b59e810b
Pedro Avalos <pavalos6401@gmail.com>
2023-01-25 21:37:57 -0600

    fix emacs config

---
+++ emacs.d/init.el
@@ -0,0 +1,20 @@
+;; author Pedro Avalos
+;; file: init.el
+
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(evil-tutor general evil-collection evil doom-modeline all-the-icons use-package no-littering)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
