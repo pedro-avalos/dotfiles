bf85349a9d20ab586b218e09dbabd37b3bcdf404
Pedro Avalos <pedro.avalos@proton.me>
2023-02-25 23:25:11 -0600

    update floating_rules to use Match

--- config/qtile/src/layouts.py
+++ config/qtile/src/layouts.py
@@ -1,6 +1,7 @@
 """Provides the layouts (and settings for them) that qtile will use."""
 
 from libqtile import layout
+from libqtile.config import Match
 
 from .util.theme import LayoutsTheme
 
@@ -30,24 +31,40 @@ def make_floating_layout(layouts_theme: LayoutsTheme) -> layout.Floating:
     """Creates the floating layout used."""
 
     return layout.Floating(
-        auto_float_typesR=[
-            # Run the utility `xprop` to see the wm class and name of an X client
-            {"wmclass": "confirm"},
-            {"wmclass": "dialog"},
-            {"wmclass": "download"},
-            {"wmclass": "error"},
-            {"wmclass": "file_progress"},
-            {"wmclass": "notification"},
-            {"wmclass": "notify"},
-            {"wmclass": "popup_menu"},
-            {"wmclass": "splash"},
-            {"wmclass": "toolbar"},
-            {"wmclass": "confirmreset"},  # gitk
-            {"wmclass": "makebranch"},  # gitk
-            {"wmclass": "maketag"},  # gitk
-            {"wmclass": "branchdialog"},  # gitk
-            {"wmclass": "pinentry"},  # GPG key password entry
-            {"wmclass": "ssh-askpass"},  # ssh-askpass
+        float_rules=[
+            Match(title="Open File"),
+            Match(title="File Operation Progress", wm_class="thunar"), # Wayland
+            Match(wm_class="Arandr"),
+            Match(wm_class="org.kde.ark"),
+            Match(wm_class="confirm"),
+            Match(wm_class="dialog"),
+            Match(wm_class="download"),
+            Match(wm_class="error"),
+            Match(wm_class="fiji-Main"),
+            Match(wm_class="file_progress"),
+            Match(wm_class="imv"),
+            Match(wm_class="lxappearance"),
+            Match(wm_class="mpv"),
+            Match(wm_class="notification"),
+            Match(wm_class="notify"),
+            Match(wm_class="popup_menu"),
+            Match(wm_class="splash"),
+            Match(wm_class="pavucontrol"),
+            Match(wm_class="Pinentry-gtk-2"), # GPG key password entry
+            Match(wm_class="pinentry"),  # GPG key password entry
+            Match(wm_class="qt5ct"),
+            Match(wm_class="ssh-askpass"),
+            Match(wm_class="Dragon-drag-and-drop"),
+            Match(wm_class="toolbar"),
+            Match(wm_class="wlroots"),
+            Match(wm_class="Xephyr"),
+            Match(wm_class="confirmreset"),  # gitk
+            Match(wm_class="makebranch"),  # gitk
+            Match(wm_class="maketag"),  # gitk
+            Match(wm_type="dialog"),
+            Match(role="gimp-file-export"),
+            Match(func=lambda c: c.has_fixed_size()),
+            Match(func=lambda c: bool(c.is_transient_for())),
         ],
         **layouts_theme.floating,
     )
