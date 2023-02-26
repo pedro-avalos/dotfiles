5fca47f5fb65afebb32e27293b5de4cc5d6a0075
Pedro Avalos <pedro.avalos@proton.me>
2023-02-26 13:12:46 -0600

    fix bar.sh

--- dwm/bar.sh
+++ dwm/bar.sh
@@ -7,24 +7,24 @@ DIR=$( dirname "${LOC}" )
 export SEPL=""
 export SEPR=" | "
 
+# Source modules
 . "${DIR}/modules/battery.sh"
 . "${DIR}/modules/nm_network.sh"
 
-# Run these in parallel
+# Run these updates in parallel
 parallelize() {
 	while true; do
 		nm_network &
 		sleep 5s
 	done
 }
-
 parallelize &
 
 # Update status bar
 while true ; do
 	top=""
 	top="${top}$( battery )"
-	top="${top}$( __NM_NETWORK__ )"
+	top="${top}${__NM_NETWORK__}"
 
 	xsetroot -name "${top}"
 	sleep 1s
