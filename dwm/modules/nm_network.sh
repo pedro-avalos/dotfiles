2872cf144e6473392019ab36b2ca26dd49176358
Pedro Avalos <pedro.avalos@proton.me>
2023-02-26 19:54:44 -0600

    Fix nm_network.sh in dwm bar

--- dwm/modules/nm_network.sh
+++ dwm/modules/nm_network.sh
@@ -2,7 +2,7 @@
 # File: nm_network.sh
 
 nm_network() {
-	CONNAME=$( nmcli -a | grep 'Wired Connection' | awk 'NR==1{print $1}' )
+	CONNAME=$( nmcli -a | grep 'Wired connection' | awk -F: '{print $1}' )
 	if [ "${CONNAME}" = "" ] ; then
 		CONNAME=$( nmcli -t -f active,ssid dev wifi | grep '^yes' | cut -c 5- )
 	fi
