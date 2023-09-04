aa206ade1df1cede9bfe710f0be6f87e195a54a9
Pedro Avalos <pedro.avalos@proton.me>
2023-09-04 01:12:40 -0500

    Edit emacs config

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -1,7 +1,4 @@
-;; author Pedro Avalos
-;; file: init.el
 ;; Do not edit init.el. Edit config.org instead.
-
-;; My configuration is written in org-mode
+;; My configuration is written in org-mode.
 (require 'org)
 (org-babel-load-file "~/.config/emacs/config.org")
