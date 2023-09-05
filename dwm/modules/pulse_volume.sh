ba5d26ca6ba87424fa081ab533d2557a34417fad
Pedro Avalos <pedro.avalos@proton.me>
2023-09-05 10:31:56 -0500

    indent pulse_volume.sh

--- dwm/modules/pulse_volume.sh
+++ dwm/modules/pulse_volume.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env sh
 
 pulse_volume() {
-	STATUS=$( pamixer --get-mute )
-	VOL=$( pamixer --get-volume )
+		STATUS=$( pamixer --get-mute )
+		VOL=$( pamixer --get-volume )
 
-	printf -- "%s" "${SEPL}"
-	if [ "${STATUS}" = "true" ] ; then 
-		printf -- "MUTE"
-	else
-		printf -- "VOL %s%%" "${VOL}"
-	fi
-	printf -- "%s" "${SEPR}"
+		printf -- "%s" "${SEPL}"
+		if [ "${STATUS}" = "true" ] ; then 
+				printf -- "MUTE"
+		else
+				printf -- "VOL %s%%" "${VOL}"
+		fi
+		printf -- "%s" "${SEPR}"
 }
 
 pulse_volume
