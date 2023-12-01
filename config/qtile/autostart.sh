8d51c3cf5c6d332bba1002b8dede395537f3d2ae
Pedro Avalos <pedro.avalos@proton.me>
2023-12-01 13:02:17 -0600

    edit udiskie options

--- config/qtile/autostart.sh
+++ config/qtile/autostart.sh
@@ -4,13 +4,14 @@ light-locker --lock-on-lid &
 nm-applet --indicator &
 blueman-applet &
 dunst &
-udiskie --appindicator &
 xfce4-clipman &
 
 if [ "${XDG_SESSION_TYPE}" = "x11" ] ; then
     redshift-gtk &
     mkdir -p "${HOME}/.local/share/picom"
     picom --daemon --log-file "${HOME}/.local/share/picom"
+    udiskie --no-automount --tray --appindicator &
 elif [ "${XDG_SESSION_TYPE}" = "wayland" ] ; then
     gammastep-indicator &
+    udiskie --no-automount --tray --appindicator --menu-update-workaround &
 fi
