a192f1a140dae7a095a7cc799a78bf13e9b3e4fe
Pedro Avalos <pedro.avalos@proton.me>
2023-12-01 13:04:19 -0600

    automount with udiskie

--- config/qtile/autostart.sh
+++ config/qtile/autostart.sh
@@ -10,8 +10,8 @@ if [ "${XDG_SESSION_TYPE}" = "x11" ] ; then
     redshift-gtk &
     mkdir -p "${HOME}/.local/share/picom"
     picom --daemon --log-file "${HOME}/.local/share/picom"
-    udiskie --no-automount --tray --appindicator &
+    udiskie --tray --appindicator &
 elif [ "${XDG_SESSION_TYPE}" = "wayland" ] ; then
     gammastep-indicator &
-    udiskie --no-automount --tray --appindicator --menu-update-workaround &
+    udiskie --tray --appindicator --menu-update-workaround &
 fi
