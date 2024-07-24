7cc60b099be75ffb516362fe20c54ff9a9a4351c
Pedro Avalos <pedro.avalos@proton.me>
2024-07-24 11:54:23 -0500

    Update zsh aliases

--- config/zsh/alias.zsh
+++ config/zsh/alias.zsh
@@ -5,14 +5,18 @@ alias resource='source $ZDOTDIR/.zshrc'
 
 # Directory-related aliases
 alias ls='ls --color=auto -F'
-alias l='ls --color=auto -lh'
-alias la='ls --color=auto -a'
-alias ll='ls --color=auto -lha'
+alias l='ls --color=auto -CF'
+alias la='ls --color=auto -A'
+alias ll='ls --color=auto -alF'
 alias grep='grep --color=auto'
 alias egrep='grep --color=auto -E'
 alias fgrep='grep --color=auto -F'
 alias diff='diff --color=auto'
 alias ip='ip --color=auto'
+
+# Add an "alert" alias for long running commands.  Use like so:
+#   sleep 10; alert
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
 # Editor-related aliases
 alias emacs_tui='emacsclient -ta ""'
