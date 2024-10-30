e74d017ac6e4f1a769a758305d5fb93567181fe2
Pedro Avalos Jimenez <pedro.avalosjimenez@canonical.com>
2024-10-30 22:53:42 +0100

    Fix LXD docker script

--- bin/fix_lxd_docker.sh
+++ bin/fix_lxd_docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # See https://documentation.ubuntu.com/lxd/en/latest/howto/network_bridge_firewalld/#prevent-connectivity-issues-with-lxd-and-docker
-iptables  -I DOCKER-USER -i lxdbr0 -j ACCEPT
-ip6tables -I DOCKER-USER -i lxdbr0 -j ACCEPT
-iptables  -I DOCKER-USER -o lxdbr0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-ip6tables -I DOCKER-USER -o lxdbr0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+sudo iptables  -I DOCKER-USER -i lxdbr0 -j ACCEPT
+sudo ip6tables -I DOCKER-USER -i lxdbr0 -j ACCEPT
+sudo iptables  -I DOCKER-USER -o lxdbr0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+sudo ip6tables -I DOCKER-USER -o lxdbr0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
